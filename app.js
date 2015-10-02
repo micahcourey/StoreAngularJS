6ab850d3a4d3f4067b03bfe307ede9fce72add21
micahcourey <micahjcourey@gmail.com>
2015-10-01 20:24:37 -0700

    add store controller and store product properties and display object in index.html

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-var app = angular.module('store', [ ]);
+(function(){
+  var app = angular.module('store', [ ]);
+
+  app.controller('StoreController', function(){
+    this.product = guitar;
+  });
+
+  var guitar = {
+    name: 'Martin D16',
+    price: 2799.00,
+    description: "The D-16 Adirondack acoustic guitar fills the room with a rich acoustic tone. Constructed with a solid Adirondack spruce top and finished in a polished gloss that perfectly complements its solid genuine mahogany back and sides, this pairing of balanced tonewoods adds just the right amount of warmth to the sound. The D-16 Adirondack is truly a guitar you'll want to pass along for generations.",
+    canPurchase: false
+  }
+})();
