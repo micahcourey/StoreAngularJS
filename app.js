9a9779f04bcc22187be39a652530b19a2cc35274
micahcourey <micahjcourey@gmail.com>
2015-10-03 22:52:55 -0700

    add tabs for description, specifications and reviews to products

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 (function(){
   var app = angular.module('store', [ ]);
 
+  app.controller("PanelController", function(){
+    this.tab = 1;
+
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+    this.isSelected = function(checkTab){
+      return this.tab === checkTab;
+    };
+  });
+
   app.controller('StoreController', function(){
     this.products = guitars;
   });
